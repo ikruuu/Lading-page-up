406b3fd4071f54e7f56522b81b76ff47da1a4bf1
ikaro almeida <seupaizinho123@gmail.com>
2023-09-21 15:55:33 -0300

    término da parte de cima da landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function imageSlider(anything){
     document.querySelector('.img__content').src = anything;
 }
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+    const starbucks = document.querySelector('.starbucks');
+    starbucks.style.color = color;
+    const learn__more = document.querySelector('.learn__more');
+    learn__more.style.background = color;
+    const left__circle2 = document.querySelector('.left__circle2');
+    left__circle2.style.background = color;
+}
