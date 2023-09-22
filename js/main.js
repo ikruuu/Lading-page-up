b5ca92cb037f038d9130e141a1212074219ea9bb
kauas3228 <kauas3228@gmail.com>
2023-09-22 14:58:03 -0300

    Mobile Menu

--- js/main.js
+++ js/main.js
@@ -10,4 +10,17 @@ function changeCircleColor(color){
     learn__more.style.background = color;
     const nav = document.querySelector('.nav');
     nav.style.background = color;
+    const mobile_menu = document.querySelector('.nav__list');
+    mobile_menu.style.background = color;
 }
+const menu = document.querySelector('.mobile_menu');
+const list = document.querySelector('.nav__list');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+menu.addEventListener('click', () =>{
+    list.classList.toggle('active')
+    line1.classList.toggle('active')
+    line2.classList.toggle('active')
+    line3.classList.toggle('active')
+})
